b4d9ac6ceae3c0baf0089bd6c544f1fbae5e2b4f
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-24 22:51:49 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -104,13 +104,13 @@ if selected == 'About':
 
         # Display the conversation history with icons
         for chat in st.session_state.conversation:
-            col1, col2 = st.columns([1, 12])
+            col1, col2 = st.columns([1, 14])
             with col1:
                 st.image(user_icon_url, width=30)
             with col2:
                 st.write(f"**User:** {chat['user']}")
             
-            col1, col2 = st.columns([1, 12])
+            col1, col2 = st.columns([1, 14])
             with col1:
                 st.image(bot_icon_url, width=30)
             with col2:

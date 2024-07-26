7152457267b2ada8fe5eba948172eb658960c8b4
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-26 13:39:39 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -149,29 +149,32 @@ if selected == 'About':
                  """
         st.title("William's AI Bot")
         
+        st.chat_message("assistant").markdown("Hi:wave:")
+        # Initialize chat history
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+
         # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
 
         # React to user input
-        prompt = st.chat_input("What is up?")
-        if prompt:
-            # Display user message in chat message container
-            st.chat_message("user").markdown(prompt)
-            # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            
-            user_question = anas_persona + prompt
-            response = model.generate_content(user_question)
-            text_response = response.text
-            
-            # Display assistant response in chat message container
-            with st.chat_message("assistant"):
-                st.write(text_response)
-            
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": text_response})
+            if prompt := st.chat_input("What is up?"):
+        
+                # Display user message in chat message container
+                st.chat_message("user").markdown(prompt)
+                # Add user message to chat history
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                user_question = anas_persona + prompt
+                response = model.generate_content(user_question)
+                        text_response = response.text
+        # Display assistant response in chat message container
+                with st.chat_message("assistant"):
+                    st.write(text_response)
+        # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": text_response})
 
                         
         st.write('---')

725ade4364d0e1baabece88d0c730e3623a3b1b4
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-26 15:46:05 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -206,19 +206,22 @@ if selected == 'About':
 
 if selected == 'AI Assistant':
     with st.container():
+
         # Define the URLs
         education_url = "https://williamlu.streamlit.app/~/+/#education"
         experience_url = "https://williamlu.streamlit.app/~/+/#experience"
         skills_url = "https://williamlu.streamlit.app/~/+/#my-skills"
         hobbies_url = "https://williamlu.streamlit.app/~/+/#my-hobbies-interests"
+        projects_url = "https://williamlu.streamlit.app/~/+/#projects"
         contact_url = "https://williamlu.streamlit.app/~/+/#b949ecef"
         socials_url = "https://williamlu.streamlit.app/~/+/#socials"
-        
+
         # Create hyperlinks using Markdown
         education_link = f"[Education]({education_url})"
         experience_link = f"[Experience]({experience_url})"
         skills_link = f"[My Skills]({skills_url})"
         hobbies_link = f"[My Hobbies & Interests]({hobbies_url})"
+        projects_link = f"[Projects]({projects_url})"
         contact_link = f"[Contact]({contact_url})"
         socials_link = f"[Socials]({socials_url})"
 
@@ -228,8 +231,8 @@ if selected == 'AI Assistant':
         Answer as if you are responding. Don't answer in second or third person.
         If you don't know the answer, you simply say "That's a secret."
         Here is more info about William: 
-        
-        About William Lu: 
+
+        **About William Lu:** 
         William Lu is a motivated student at NYU Tandon Bridge Program with experience 
         in product design and development, CAD modeling, assembling, QA testing, computer vision, 
         machine learning object detection, data analysis, and seeking opportunities in medical imaging, 
@@ -248,20 +251,20 @@ if selected == 'AI Assistant':
         **Languages:** Chinese (Native), and Spanish (Elementary Proficiency, basic, entry-level) 
         **Others:** Good verbal and written communication skills, 3D printing, assembling, troubleshooting, validation and documentation, FEA and failure analysis, and technical writing
         - You can also find that information in the {skills_link} section of the "About" tab of my AI portfolio website.
-        
+
         ### HOBBIES:
         I enjoy most of my time listening to music, watching YouTube videos, WeChat and TikTok short videos, 
         and reading journal articles or magazines in the fields of physics, engineering, medicine, and biology 
         to gain insight for research. 
         - You can also find that information in the {hobbies_link} section of the "About" tab of my AI portfolio website.
-
+        
         ### PROJECTS:
         You can find information about my projects in a few places:
         - William's Github: [https://github.com/williaml12](https://github.com/williaml12)
         - Hackster.io: [https://www.hackster.io/wlu1](https://www.hackster.io/wlu1)
         - Go to "Projects" tab of my AI portfolio website
         - You can find links to those websites in the {socials_link} section of the "About" tab of my AI portfolio website.
-        
+
         ### CONTACT:
         You can reach me at luwei2359@gmail.com. You can also find me on LinkedIn at [https://www.linkedin.com/in/william-lu-47693b145/](https://www.linkedin.com/in/william-lu-47693b145/). 
         - Or you can contact me through the {contact_link} tab of my AI portfolio website.
@@ -269,7 +272,7 @@ if selected == 'AI Assistant':
         ### AVAILABILITY:
         William is actively seeking new opportunities and is ready to start immediately.
         """
-        
+
         st.title("William's AI Bot")
         
        # Initialize session state for conversation history if not already done

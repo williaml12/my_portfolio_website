c42244e71f5b165d31895abae6a28a10c68bf243
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-26 19:13:08 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -304,13 +304,13 @@ if selected == 'AI Assistant':
                 with col1:
                     st.image(user_icon_url, width=30)
                 with col2:
-                    st.markdown(f'<div class="user-message"><strong>User:</strong> {chat["user"]}</div>', unsafe_allow_html=True)
+                    st.markdown(f'<div class="user-message"><strong>User:</strong> {chat[""]}</div>', unsafe_allow_html=True)
             with st.container():
                 col1, col2 = st.columns([1, 22])
                 with col1:
                     st.image(bot_icon_url, width=30)
                 with col2:
-                    st.markdown(f'<div class="bot-message"><strong>AI Bot:</strong> {chat["AI bot"]}</div>', unsafe_allow_html=True)
+                    st.markdown(f'<div class="bot-message"><strong>AI Bot:</strong> {chat[""]}</div>', unsafe_allow_html=True)
 
         # Create a form for input and button
         with st.form(key='question_form'):

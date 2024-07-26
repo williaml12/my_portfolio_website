a474a836b20640fe293d8d830996f33f795d1cd4
William Lu <32563160+williaml12@users.noreply.github.com>
2024-07-26 15:57:41 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -208,7 +208,7 @@ if selected == 'AI Assistant':
     with st.container():
 
         persona = """
-                "You are William's AI bot". You help people answer questions about your self (i.e William)
+                You are William's AI bot. You help people answer questions about your self (i.e William)
                 Answer as if you are responding . Dont answer in second or third person.
                 If you don't know they answer you simply say "That's a secret"
                 Here is more info about William: 
@@ -265,7 +265,7 @@ if selected == 'AI Assistant':
                 
                 Availability:
                 William is actively seeking new opportunities and is ready to start immediately.
-                 """
+                """
 
 
         st.title("William's AI Bot")

e3524fa74dc90e59d136a787525c4daf51edbe17
William Lu <32563160+williaml12@users.noreply.github.com>
2024-08-07 19:58:55 -0400

    Update my_portfolio_website.py

--- my_portfolio_website.py
+++ my_portfolio_website.py
@@ -31,7 +31,7 @@ if selected == 'About':
                      "machine learning object detection, and data analysis. Seeking opportunities to contribute to "
                      "cutting-edge research in fluid mechanics, aerodynamics, and aerospace technology."
                     )
-            st.write("Get in touch by emailing me at luwei2359@gmail.com")
+            # st.write("Get in touch by emailing me at luwei2359@gmail.com")
             # if st.button("Get In Touch"):
             #     st.write("luwei2359@gmail.com")
             st.link_button("Get In Touch", "https://mail.google.com/mail/u/0/#inbox?compose=jrjtXFBwbVZkBXGcjzJPXMSlVZbjjnCzNSLFGQLfWwbqXPWrBVPbQxbTxcCnkRZqzWxFSRhT")
